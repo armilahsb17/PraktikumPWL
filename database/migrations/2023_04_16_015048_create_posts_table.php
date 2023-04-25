ab22b95dcb049e12af57f7a9f5af46e5d5901077
unknown <armilasakinahhasibuan@gmail.com>
2023-04-25 15:41:20 +0700

    tugas 2

--- database/migrations/2023_04_16_015048_create_posts_table.php
+++ database/migrations/2023_04_16_015048_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string("title, 100");
+            $table->string("title", 100);
             $table->text("excerpt");
             $table->text("content");
             $table->string("image");
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this -> call(usersTableSeeder::class);
+        $this -> call(PostsTableSeeder::class);
     }
 }
